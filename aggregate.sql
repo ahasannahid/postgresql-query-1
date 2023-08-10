9a6f601318f114859ca562c0b46c1adc836e6101
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-11 00:56:36 +0600

    aggregate function

---
+++ aggregate.sql
@@ -0,0 +1,25 @@
+-- Active: 1691480676960@@127.0.0.1@5432@test2
+-- aggregate function
+SELECT * from departments;
+
+-- avg salary
+-- column name change korake alaiceing bole
+SELECT AVG(salary) as something FROM employees;
+SELECT MAX(salary) as something FROM employees;
+SELECT MIN(salary) as something FROM employees;
+SELECT MIN(salary) as something FROM employees;
+
+-- group BY
+
+SELECT deptid, MAX(salary)  FROM employees GROUP BY deptid;
+SELECT deptid, MIN(salary) FROM employees GROUP BY deptid;
+SELECT MIN(salary) FROM employees GROUP BY deptid;
+SELECT AVG(salary) FROM employees GROUP BY deptid;
+
+SELECT d.name, AVG(e.salary),MAX(e.salary), MIN(e.salary), SUM(e.salary) from employees e
+FULL JOIN departments d on e.deptid = d.deptid
+GROUP BY d.name;
+
+SELECT d.name, AVG(e.salary),MAX(e.salary), MIN(e.salary), SUM(e.salary), count(*) from departments d
+FULL JOIN employees e on e.deptid = d.deptid
+GROUP BY d.deptid;
